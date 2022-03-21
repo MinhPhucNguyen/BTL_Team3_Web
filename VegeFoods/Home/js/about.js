9cafcd22427d78ed440c566b4ddf5c58e1df8922
NXMinh2k2 <minhxuannguyen0809@gmail.com>
2022-03-21 21:01:30 +0700

    Minh sua file about.html

---
+++ VegeFoods/Home/js/about.js
@@ -0,0 +1,32 @@
+var listCounterup = document.querySelectorAll('.counter-up')
+
+function counter(el) {
+    var numberEl = el.querySelector('.number')
+    var to = parseInt(numberEl.innerText)
+    var count = 0
+    var time = 1000;
+    var step = to / time
+    var counting = setInterval(function() {
+        count += step
+        if(count > to) {
+            clearInterval(counting)
+            numberEl.innerText = to
+        } else {
+            numberEl.innerText = Math.ceil(count)
+        }
+    }, 1)
+}
+
+listCounterup.forEach(function(item) {
+    counter(item)
+})
+
+
+var circle = document.querySelectorAll('.circle')
+circle.forEach(function(item) {
+    item.addEventListener('click', function() {
+
+    })
+})
+
+
