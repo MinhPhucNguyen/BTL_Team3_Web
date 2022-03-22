6c5d798041f4f8999c862c8cfe03975ff905c44d
MinhPhucNguyen <2051063667@e.tlu.edu.vn>
2022-03-22 12:29:35 +0700

    Phuc up lai code css about.css

--- VegeFoods/Home/js/index.js
+++ VegeFoods/Home/js/index.js
@@ -1,10 +1,53 @@
+// const headerContainer = document.getElementsByClassName("header__container");
+// const linksContainer = document.querySelector(".header__nav");
+
+// window.addEventListener("scroll", function () {
+//   const scrollHeight = window.pageYOffset;
+//   const navHeight = headerContainer.getBoundingClientRect().height;
+//   if (scrollHeight > navHeight) {
+//     header.classList.add("fixed-header");
+//   } 
+//   else {
+//     headerContainer.classList.remove("fixed-header");
+//   }
+// });
+
+// const scrollLinks = document.querySelectorAll(".header__nav--link");
+// scrollLinks.forEach((link) => {
+//   link.addEventListener("click", (e) => {
+//     // prevent default
+//     e.preventDefault();
+//     // navigate to specific spot
+//     const id = e.currentTarget.getAttribute("href").slice(1);
+//     const element = document.getElementById(id);
+
+//     const navHeight = headerContainer.getBoundingClientRect().height;
+//     const containerHeight = linksContainer.getBoundingClientRect().height;
+//     const fixedNav = headerContainer.classList.contains("fixed-header");
+//     let position = element.offsetTop - navHeight;
+
+//     if (!fixedNav) {
+//       position = position - navHeight;
+//     }
+//     if (navHeight > 82) {
+//       position = position + containerHeight;
+//     }
+
+//     window.scrollTo({
+//       left: 0,
+//       top: position,
+//     });
+//     // close
+//     linksContainer.style.height = 0;
+//   });
+// });
+
+
 var get_Day = document.querySelector("#days")
 var get_Hours = document.querySelector("#hours")
 var get_Minutes = document.querySelector("#minutes")
 var get_Second = document.querySelector("#second")
-
 var DateCountDowntext = document.querySelectorAll(".time .time__text")
-
 var countDownDate = new Date("Jan 5, 2024 15:37:25").getTime()
 
 function DateofIndex(){
