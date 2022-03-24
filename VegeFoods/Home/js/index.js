0df0dd0412fc3d51cbe09798f03be816e1821e06
HTN-UnHappyCODE0 <99171035+HTN-UnHappyCODE0@users.noreply.github.com>
2022-03-25 00:15:05 +0700

    Nam thêm code index.js

--- VegeFoods/Home/js/index.js
+++ VegeFoods/Home/js/index.js
@@ -5,18 +5,18 @@ var get_Second = document.querySelector("#second")
 var DateCountDowntext = document.querySelectorAll(".time .time__text")
 var countDownDate = new Date("Jan 5, 2024 15:37:25").getTime()
 
-function DateofIndex(){
+function DateofIndex() {
     // Today's date and time
     var now = new Date().getTime()
 
     var distance = countDownDate - now
-    
+
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-    get_Day.innerText= days
+
+    get_Day.innerText = days
     get_Hours.innerText = hours
     get_Minutes.innerText = minutes
     get_Second.innerText = seconds
@@ -24,3 +24,19 @@ function DateofIndex(){
 
 setInterval(DateofIndex, 1000)
 
+var getHeaderElement = document.querySelector("#header")
+
+function performHeader() {
+    document.onscroll = function() {
+        const scrollHeight = window.scrollY
+
+        if (scrollHeight > 400) {
+            getHeaderElement.style.position = "fixed"
+            getHeaderElement.style.zIndex = "3"
+        } else if (scrollHeight < 400) {
+            getHeaderElement.style.position = "unset"
+        }
+    }
+}
+
+performHeader()
