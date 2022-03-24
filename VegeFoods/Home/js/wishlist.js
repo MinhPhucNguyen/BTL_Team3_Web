4819f3508ba2c481e4ecd0e60b5187c5d190ac51
MinhPhucNguyen <2051063667@e.tlu.edu.vn>
2022-03-24 20:04:01 +0700

    Phuc up code sua file wishlish.html

--- VegeFoods/Home/js/wishlist.js
+++ VegeFoods/Home/js/wishlist.js
@@ -1,19 +1,15 @@
-var listPrice = document.querySelectorAll(".product__price .price");
-var listQuantity = document.querySelectorAll(".input__group input[type=text]");
-var listTotal = document.querySelectorAll(".total span");
+var listPrice = document.querySelector(".product__price .price");
+var listQuantity = document.querySelector(".input__group input[type=text]");
+var listTotal = document.querySelector(".total span");
 
 var listTr = document.querySelectorAll("tr")
 
 
-const setTotal = ()=>{
-    listTr.forEach(function(item){
-        let get_price = parseFloat(item.querySelector(".product__price .price").innerText) 
-        console.log(item.querySelector(".product__price .price"))
-        let get_quantity = item.querySelector(".input__group input[type=text]").value
-        console.log(get_quantity)
-        let total = item.querySelector(".total span") 
-        total.innerText = (Math.round((get_price * get_quantity)*100)/100).toFixed(2)
-    })
+function setTotal() {
+    var getPrice = parseFloat(listPrice.innerText)
+    var getQuantity = listQuantity.value
+    var setTotal = (Math.round((getPrice * getQuantity)*100)/100).toFixed(2)
+    listTotal.innerHTML = setTotal
 }
 
 setTotal()
