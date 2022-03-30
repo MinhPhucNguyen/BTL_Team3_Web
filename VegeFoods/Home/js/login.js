afd9cc146f2a7ec28ba62f4c0154bc5f8d0a687e
NXMinh2k2 <minhxuannguyen0809@gmail.com>
2022-03-30 20:50:20 +0700

    minh vua them file login.html

--- VegeFoods/Home/js/login.js
+++ VegeFoods/Home/js/login.js
@@ -22,7 +22,7 @@ const localPassword = localStorage.getItem('password')
 function changePage() {
     window.location.assign('http://127.0.0.1:5500/contact.html')
 }
-changePage()
+// changePage()
 
 // formID.onsubmit = function() {
 //     if(nameValue === localName && passwordValue === localPassword) {
