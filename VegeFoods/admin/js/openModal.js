f150e583da9fedf0c1f923c43a3e71cc1a2dee23
MinhPhucNguyen <99171076+MinhPhucNguyen@users.noreply.github.com>
2022-04-01 20:32:38 +0700

    Sua openModal.js

--- VegeFoods/admin/js/openModal.js
+++ VegeFoods/admin/js/openModal.js
@@ -1,10 +1,15 @@
 var imageModal = document.querySelector(".modal__add-img")
 var openModalBtn = document.querySelector(".add__img-btn")
 var modalContainer = document.querySelector(".modal-container")
+var addImg = document.querySelector(".add__img")
 
 function toggleModal() {
     imageModal.classList.toggle("hide")
 }
+
+addImg.addEventListener("click",function() {
+    toggleModal()
+})
 
 openModalBtn.addEventListener("click",function(){
     toggleModal()
