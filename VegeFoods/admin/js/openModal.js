e34eeaf9d79a1bd1eb55bb956261caf065a13961
MinhPhucNguyen <99171076+MinhPhucNguyen@users.noreply.github.com>
2022-04-02 00:09:42 +0700

    Up sua file product.html va product.css

--- VegeFoods/admin/js/openModal.js
+++ VegeFoods/admin/js/openModal.js
@@ -1,9 +1,9 @@
+// Open Image Modal
 var imageModal = document.querySelector(".modal__add-img")
 var openModalBtn = document.querySelector(".add__img-btn")
 var modalContainer = document.querySelector(".modal-container")
 var addImg = document.querySelector(".add__img")
 var closeIconModal = document.querySelector(".modal-close-icon")
-
 function toggleModal() {
     imageModal.classList.toggle("hide")
 }
@@ -25,3 +25,30 @@ imageModal.addEventListener("click", function(e){
         toggleModal()
     }
 })
+
+
+
+// Open Edit Product
+var editWrapper = document.querySelector(".edit-wrapper")
+var editBtn = document.querySelector(".button-add")
+var backBtn = document.querySelector(".return__btn")
+function editProductToggle() {
+    editWrapper.classList.toggle("wrapper-none")
+}
+
+backBtn.addEventListener("click",function () {
+    editProductToggle() 
+})
+
+editBtn.addEventListener("click", function(){
+    editProductToggle() 
+})
+
+editWrapper.addEventListener("click", function(e) {
+    if(e.target == e.currentTarget){
+        editProductToggle()
+    }
+})
+
+
+
