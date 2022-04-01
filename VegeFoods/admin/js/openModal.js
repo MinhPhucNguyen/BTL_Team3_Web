4cbb370d78b793bd0e9f12d7c807726bda4f9f88
MinhPhucNguyen <99171076+MinhPhucNguyen@users.noreply.github.com>
2022-04-01 21:13:59 +0700

    Up hoan thien modalimg

--- VegeFoods/admin/js/openModal.js
+++ VegeFoods/admin/js/openModal.js
@@ -2,10 +2,15 @@ var imageModal = document.querySelector(".modal__add-img")
 var openModalBtn = document.querySelector(".add__img-btn")
 var modalContainer = document.querySelector(".modal-container")
 var addImg = document.querySelector(".add__img")
+var closeIconModal = document.querySelector(".modal-close-icon")
 
 function toggleModal() {
     imageModal.classList.toggle("hide")
 }
+
+closeIconModal.addEventListener("click", function() {
+    toggleModal()
+})
 
 addImg.addEventListener("click",function() {
     toggleModal()
