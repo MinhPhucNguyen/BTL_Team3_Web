794fd05c7e0eecaead5d4933eacc91b7798d55b8
NXMinh2k2 <minhxuannguyen0809@gmail.com>
2022-04-02 16:36:50 +0700

    Minh vua them code file product.js

--- VegeFoods/admin/js/product.js
+++ VegeFoods/admin/js/product.js
@@ -4,6 +4,8 @@ const nameInput = document.querySelector('.input__name-product')
 const priceInput = document.querySelector('.input__price-product')
 const quantityInput = document.querySelector('.input__promotional-price-product')
 const saveBtn = document.querySelector('.save-btn')
+const deleteAllBtn = document.querySelector('.button-delete')
+console.log(deleteAllBtn)
 
 // const idValue = idInput.value
 // const nameValue = nameInput.value 
@@ -20,32 +22,60 @@ function add() {
         price: priceInput.value ,
         quantity: quantityInput.value 
     }
-    
+
     listProduct.push(product)
     console.log(listProduct)
 }
 
+
 function show() {
+    var tbody = ``
     listProduct.forEach(function(product) {
-        tbodyElement.innerHTML = `<tr class="tr2 tr-similar">
-                                <td>${product.id}</td>
-                                <td></td>
-                                <td>${product.name}</td>
-                                <td>${product.price}</td>
-                                <td>${product.quantity}</td>
-                                <td>
-                                    <div class="delete">
-                                        <i class="fa-solid fa-circle-xmark"></i>
-                                    </div>
-                                </td>
-                                `
+        tbody += `<tr class="tr2 tr-similar">
+        <td>${product.id}</td>
+        <td></td>
+        <td>${product.name}</td>
+        <td>${product.price}</td>
+        <td>${product.quantity}</td>
+        <td>
+            <div class="edit-wrap">
+                <div class="delete" onclick = "deleteItem(${product.id})">
+                    <i class="fa-solid fa-circle-xmark"></i>
+                </div>
+                <div class="edit">
+                    <i class="fa-solid fa-marker"></i>
+                </div>
+            </div>
+        </td>
+        `
+        tbodyElement.innerHTML = tbody
     })
+}
+
+function clear() {
+   idInput.value = ""
+   nameInput.value = ""
+   priceInput.value = ""
+   quantityInput.value = ""
+}
+
+function deleteItem(ID) {
+    for(let i in listProduct) {
+        if(listProduct[i].id == ID) {
+            listProduct.splice(i,1)
+            show()
+        }
+    }
+    console.log(ID)
 }
 
 saveBtn.onclick = function() {
     editWrapper.classList.add("wrapper-none")
     add() 
-    show()       
+    show()      
+    clear() 
 }
 
-
+deleteAllBtn.onclick = function() {
+    tbodyElement.innerHTML = ""
+}
