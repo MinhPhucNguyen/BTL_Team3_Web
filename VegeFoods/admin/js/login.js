0186a70e6d81af99548fb2800c4ff67714154ba7
MinhPhucNguyen <99171076+MinhPhucNguyen@users.noreply.github.com>
2022-04-04 09:35:10 +0700

    Sua file product.html

--- VegeFoods/admin/js/login.js
+++ VegeFoods/admin/js/login.js
@@ -1,16 +1,16 @@
-var nameValue = document.querySelector('#fullname').value
-var passwordValue = document.querySelector('#password').value
-var button = document.querySelector('button')
-var formMessage1 = document.querySelector('.form-message1')
-var formMessage2 = document.querySelector('.form-message2')
-var formID = document.getElementById('form-1')
+// var nameValue = document.querySelector('#fullname').value
+// var passwordValue = document.querySelector('#password').value
+// var button = document.querySelector('button')
+// var formMessage1 = document.querySelector('.form-message1')
+// var formMessage2 = document.querySelector('.form-message2')
+// var formID = document.getElementById('form-1')
 
 
-localStorage.setItem('username', 'Nguyen Xuan Minh')
-localStorage.setItem('password', '123456')
+// localStorage.setItem('username', 'Nguyen Xuan Minh')
+// localStorage.setItem('password', '123456')
 
-const localName = localStorage.getItem('username')
-const localPassword = localStorage.getItem('password')
+// const localName = localStorage.getItem('username')
+// const localPassword = localStorage.getItem('password')
 
 
 // button.addEventListener('click', function(e) {
@@ -19,15 +19,13 @@ const localPassword = localStorage.getItem('password')
 //         window.location.assign('123.html')
 //     }
 // })
-function changePage() {
-   location.href = 'http://127.0.0.1:5500/dashboard.html'
+// function changePage() {
+//    location.href = 'http://127.0.0.1:5500/dashboard.html'
 // changePage()
 
 // formID.onsubmit = function() {
 //     if(nameValue === localName && passwordValue === localPassword) {
-//         window.location.assign('http://127.0.0.1:5500/contact.html')    }
+//         window.location.assign('http://127.0.0.1:5500/contact.html')    
+//    }
 // }
-
-
-
 
