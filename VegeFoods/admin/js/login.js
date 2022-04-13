ed5c7234da9e2e55e942e6ef40301e2c84f9ca5b
NXMinh2k2 <minhxuannguyen0809@gmail.com>
2022-04-13 23:36:17 +0700

    Minh vua sua file login

--- VegeFoods/admin/js/login.js
+++ VegeFoods/admin/js/login.js
@@ -1,31 +1,17 @@
-// var nameValue = document.querySelector('#fullname').value
-// var passwordValue = document.querySelector('#password').value
-// var button = document.querySelector('button')
-// var formMessage1 = document.querySelector('.form-message1')
-// var formMessage2 = document.querySelector('.form-message2')
-// var formID = document.getElementById('form-1')
+var button = document.querySelector('.form-submit')
+
+window.
+
+button.addEventListener('click', function(e) {
+    e.preventDefault()
+    location.assign('http://127.0.0.1:5500/dashboard.html')
+})
 
 
 // localStorage.setItem('username', 'Nguyen Xuan Minh')
 // localStorage.setItem('password', '123456')
 
-// const localName = localStorage.getItem('username')
-// const localPassword = localStorage.getItem('password')
 
 
-// button.addEventListener('click', function(e) {
-//     e.preventDefault()
-//     if(nameValue === localName && passwordValue === localPassword) {
-//         window.location.assign('123.html')
-//     }
-// })
-// function changePage() {
-//    location.href = 'http://127.0.0.1:5500/dashboard.html'
-// changePage()
 
-// formID.onsubmit = function() {
-//     if(nameValue === localName && passwordValue === localPassword) {
-//         window.location.assign('http://127.0.0.1:5500/contact.html')    
-//    }
-// }
 
