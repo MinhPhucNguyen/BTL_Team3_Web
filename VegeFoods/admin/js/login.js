06d256766252604a0829c24636c77e3b3a3d2b36
MinhPhucNguyen <99171076+MinhPhucNguyen@users.noreply.github.com>
2022-04-14 23:01:36 +0700

    sua href trong file login.js và register.js

--- VegeFoods/admin/js/login.js
+++ VegeFoods/admin/js/login.js
@@ -9,15 +9,10 @@ const localPassword = localStorage.getItem('password')
 button.addEventListener('click', function(e) {
     e.preventDefault()
     if(nameInput.value === localName && passwordInput.value === localPassword) {
-        location.assign('http://127.0.0.1:5500/dashboard.html')
+        location.assign('http://127.0.0.1:5500/admin/dashboard.html')
     } else {
         alert("Nhập lại tài khoản và mật khẩu")
     }
 })
 
 
-<<<<<<< HEAD
-=======
-// localStorage.setItem('username', 'Nguyen Xuan Minh')
-// localStorage.setItem('password', '123456')
->>>>>>> 4e00ce051eac7421ac6421bfe74bfebc6f307071
